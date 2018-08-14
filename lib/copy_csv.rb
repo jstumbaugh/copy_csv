148f6bcbb3eb14a424f8bcccd2fa2f1ab086f362
Jason Stumbaugh <jason.stumbaugh@orm-tech.com>
2018-08-14 15:17:49 -0500

    Add copy_csv method

--- lib/copy_csv.rb
+++ lib/copy_csv.rb
@@ -1,5 +1,34 @@
 require "copy_csv/version"
 
 module CopyCsv
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    # Performs a database query to copy results as CSV to an IO object.
+    #
+    # CSV is created directly in PostgreSQL with less overhead then written to
+    # the provided IO object.
+    #
+    # Example
+    #
+    #     File.open("users.csv", "wb") do |file|
+    #       User.where(unsubscribed: false).copy_csv(io: file)
+    #     end
+    #
+    # Returns nil
+    def copy_csv(io, relation: all)
+      query = <<-SQL
+        COPY (#{ relation.to_sql }) TO STDOUT WITH DELIMITER ',' CSV HEADER ENCODING 'UTF-8' QUOTE '"'
+      SQL
+
+      raw = connection.raw_connection
+      raw.copy_data(query) do
+        while (row = raw.get_copy_data)
+          io.puts row
+        end
+      end
+
+      nil
+    end
+  end
 end
